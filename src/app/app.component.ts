f4a357f9f8f2ebda4429cbd44926b6ae1f15da63
snehal patel <snpatel.dev@gmail.com>
2022-12-16 22:06:46 -0500

    # HTML, CSS & logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-game';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  onInputChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
